2c3c3defe709887d1516ac3531b4cc38229ca0a1
prajyotsogale <psogale212@gmail.com>
2024-08-27 20:33:02 +0530

    hibernate

---
+++ AdvanceHibernate/AdvanceHibernate/src/main/java/com/example/AdvanceHibernate/AdvanceHibernateApplication.java
@@ -0,0 +1,13 @@
+package com.example.AdvanceHibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvanceHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdvanceHibernateApplication.class, args);
+	}
+
+}
---
+++ AdvanceHibernate/AdvanceHibernate/src/test/java/com/example/AdvanceHibernate/AdvanceHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AdvanceHibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvanceHibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
